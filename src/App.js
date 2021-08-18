416a58f83933f71ddca52553904c8dc7005fc544
kongruksiamza <kongruksiam123@gmail.com>
2021-08-19 02:15:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
+import CurrencyComponent from './components/CurrencyComponent'
+import money from './img/money.png'
+import {useEffect, useState} from "react"
 function App() {
+  
+
+  const [currencyChoice,setCurrencyChoice] = useState([])
+
+  const [fromCurrency,setFromCurrency] = useState("THB")
+  const [toCurrency,setToCurrency] = useState("USD")
+
+  const [amount,setAmount] = useState(1)
+  const [exChangeRate,setExchangeRate] = useState(0)
+
+  const [checkFromCurrency,setCheckFromCurrency] = useState(true)
+  let fromAmount,toAmount
+
+  if(checkFromCurrency){
+      fromAmount = amount
+      toAmount = (amount*exChangeRate).toFixed(2)
+  }else{
+      toAmount = amount
+      fromAmount = (amount/exChangeRate).toFixed(2)
+  }
+
+  useEffect(()=>{
+      const url =`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`
+      fetch(url)
+      .then(res=>res.json())
+      .then(data=>{
+        setCurrencyChoice([...Object.keys(data.rates)])
+        setExchangeRate(data.rates[toCurrency])
+      })
+  },[fromCurrency,toCurrency])
+
+  const amountFromCurrency=(e)=>{
+      setAmount(e.target.value)
+      setCheckFromCurrency(true)
+  }
+
+  const amountToCurrency=(e)=>{
+      setAmount(e.target.value)
+      setCheckFromCurrency(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <img src={money} alt="logo" className="money-img"/>
+        <h1>แอพแปลงสกุลเงิน (API)</h1>
+        <div className="container">
+          <CurrencyComponent 
+            currencyChoice={currencyChoice} 
+            selectCurrency={fromCurrency}
+            changeCurrency={(e)=>setFromCurrency(e.target.value)}
+            amount = {fromAmount}
+            onChangeAmount = {amountFromCurrency}
+          />
+          <div className="equal"> = </div>
+          <CurrencyComponent 
+            currencyChoice={currencyChoice} 
+            selectCurrency={toCurrency}
+            changeCurrency={(e)=>setToCurrency(e.target.value)}
+            amount = {toAmount}
+            onChangeAmount = {amountToCurrency}
+          />
+        </div>
+      </div>
   );
 }
 
---
+++ src/components/CurrencyComponent.js
@@ -0,0 +1,19 @@
+import './CurrencyComponent.css'
+const CurrencyComponent = (props)=>{
+    const {currencyChoice,selectCurrency,changeCurrency,amount,onChangeAmount} = props
+    return(
+        <div className="currency">
+            <select value={selectCurrency} onChange={changeCurrency}>
+                {currencyChoice.map((choice)=>
+                    <option key={choice} value={choice}>{choice}</option>
+                )}
+            </select>
+            <input type="number" 
+                value={amount}
+                onChange = {onChangeAmount}
+            />
+        </div>
+    )
+}
+
+export default CurrencyComponent
